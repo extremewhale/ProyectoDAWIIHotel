77accd91e985bbc1ef3e8f747eeab7cf9e4241ea
elifio <elifioroldan@hotmail.com>
2023-10-02 20:56:26 -0500

    Menu Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProyectoDAWIIHotel';
+
+  boton(){
+    console.log("CLic")
+    var body = document.body;
+
+    // Verifica si el cuerpo tiene la clase 'closed-menu'
+    if (body.classList.contains('closed-menu')) {
+        // Si tiene la clase, remuévela
+        body.classList.remove('closed-menu');
+    } else {
+        // Si no tiene la clase, agrégala
+        body.classList.add('closed-menu');
+    }
+  }
+
 }
